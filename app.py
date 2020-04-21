f1ca917f77f6129cb7db04118062737f6af13557
parkgwanyong <pgy9394@naver.com>
2020-04-21 18:50:05 +0900

    app.py commit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 
 # 개봉 영화 리스트 조회
 @app.route('/api/list', methods=['GET'])
-def museum_list():
+def movie_available():
     # 1. mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
     # 참고) find({},{'_id':False}), sort()를 활용하면 굿!
     stars_list = list(db.mystar.find({},{'_id':False}).sort("like",-1))
@@ -22,8 +22,19 @@ def museum_list():
     return jsonify({'result': 'success','stars_list': stars_list})
 
 # 개봉 예정 영화 리스트 조회
-@app.route('/api/like', methods=['POST'])
-def room_detail():
+@app.route('/api/like', methods=['GET'])
+def movie_unavailable():
+    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
+    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
+    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
+    # 참고: '$set' 활용하기!
+    # 5. 성공하면 success 메시지를 반환합니다.
+    return jsonify({'result': 'success','msg':'like 연결되었습니다!'})
+
+
+@app.route('/api/like', methods=['GET'])
+def counting_list():
     # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
     # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
     # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
@@ -32,6 +43,16 @@ def room_detail():
     # 5. 성공하면 success 메시지를 반환합니다.
 	return jsonify({'result': 'success','msg':'like 연결되었습니다!'})
 
+
+@app.route('/api/like', methods=['GET'])
+def counting_list():
+    # 1. 클라이언트가 전달한 name_give를 name_receive 변수에 넣습니다.
+    # 2. mystar 목록에서 find_one으로 name이 name_receive와 일치하는 star를 찾습니다.
+    # 3. star의 like 에 1을 더해준 new_like 변수를 만듭니다.
+    # 4. mystar 목록에서 name이 name_receive인 문서의 like 를 new_like로 변경합니다.
+    # 참고: '$set' 활용하기!
+    # 5. 성공하면 success 메시지를 반환합니다.
+	return jsonify({'result': 'success','msg':'like 연결되었습니다!'})
 
 
 
